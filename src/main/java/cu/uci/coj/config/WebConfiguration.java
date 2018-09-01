c2f3da01b9616425a657db5dcd14e3584c6411d2
jorgeamadosoria <darksoul.uci@gmail.com>
2018-09-01 10:31:56 -0300

    configuration file adjustments

--- src/main/java/cu/uci/coj/config/WebConfiguration.java
+++ src/main/java/cu/uci/coj/config/WebConfiguration.java
@@ -4,14 +4,23 @@
  */
 package cu.uci.coj.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 
 @Configuration
 public class WebConfiguration {
@@ -31,4 +40,34 @@ public class WebConfiguration {
         return bean;
     }
     
+    @Bean
+    public ObjectMapper objectMapper() {
+    	return new ObjectMapper();
+    }
+    
+    @Bean
+    public HttpMessageConverter<?> marshallingHttpMessageConverter() {
+    	MappingJackson2HttpMessageConverter bean = new MappingJackson2HttpMessageConverter();
+    	bean.setObjectMapper(objectMapper());
+    	return bean;
+    }
+    
+    @Bean
+    public RequestMappingHandlerAdapter adapter() {
+    	RequestMappingHandlerAdapter bean = new RequestMappingHandlerAdapter();
+    	List<HttpMessageConverter<?>> lists = new ArrayList<>();
+    	lists.add(marshallingHttpMessageConverter());
+    	bean.setMessageConverters(lists);
+    	return bean;
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+    	RestTemplate bean = new RestTemplate();
+    	List<HttpMessageConverter<?>> lists = new ArrayList<>();
+    	lists.add(marshallingHttpMessageConverter());
+    	bean.setMessageConverters(lists);
+    	return bean;
+    }
+
 }
