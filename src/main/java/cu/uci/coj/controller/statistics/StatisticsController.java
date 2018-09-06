d2ec86adf5a6cbea75c9817a037f7322173dddc3
jorgeamadosoria <darksoul.uci@gmail.com>
2018-09-05 23:56:22 -0300

    removed several maven dependencies

--- src/main/java/cu/uci/coj/controller/statistics/StatisticsController.java
+++ src/main/java/cu/uci/coj/controller/statistics/StatisticsController.java
@@ -1,28 +1,9 @@
 package cu.uci.coj.controller.statistics;
 
 import java.security.Principal;
-import java.text.NumberFormat;
-import java.util.Map;
-import java.util.Set;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import cu.uci.coj.model.UserClassificationStats;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.labels.ItemLabelAnchor;
-import org.jfree.chart.labels.ItemLabelPosition;
-import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.renderer.category.BarRenderer3D;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.ui.TextAnchor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,88 +17,88 @@ import cu.uci.coj.dao.UserDAO;
 import cu.uci.coj.model.Contest;
 import cu.uci.coj.model.Language;
 import cu.uci.coj.model.Stats;
+import cu.uci.coj.model.UserClassificationStats;
 
 @Controller
 @RequestMapping(value = "/")
-public class StatisticsController extends BaseController  {
+public class StatisticsController extends BaseController {
 
-    @Resource
-    private BaseDAO baseDAO;
-    @Resource
-    private UserDAO userDAO;
-    @Resource
-    private ContestDAO contestDAO;
+	private static final long serialVersionUID = 5068203635059987095L;
+	@Resource
+	private BaseDAO baseDAO;
+	@Resource
+	private UserDAO userDAO;
+	@Resource
+	private ContestDAO contestDAO;
 
-    private Stats createDataset(String query) {
-       return baseDAO.object(query,Stats.class);
-    }
+	private Stats createDataset(String query) {
+		return baseDAO.object(query, Stats.class);
+	}
+	//
+	// private Map<String, ?> createDataset(int cid) {
+	// Map<String, ?> map = baseDAO.map("statistics.contest.status", cid);
+	// Set<String> set = map.keySet();
+	// for (String key : set) {
+	// if (!key.equals("total")) {
+	// double val = (Long) map.get(key);
+	// val /= (Long) map.get("total");
+	// val *= 100;
+	// val = (double) Math.round(val * 100) / 100;
+	// map.addValue(val, "status", key.toLowerCase());
+	// }
+	// }
+	// return map;
+	// }
 
-    private CategoryDataset createDataset(int cid) {
-        DefaultCategoryDataset defaultcategorydataset = new DefaultCategoryDataset();
-        Map<String, ?> map = baseDAO.map("statistics.contest.status", cid);
-        Set<String> set = map.keySet();
-        for (String key : set) {
-            if (!key.equals("total")) {
-                double val = (Long) map.get(key);
-                val /= (Long) map.get("total");
-                val *= 100;
-                val = (double) Math.round(val * 100) / 100;
-                defaultcategorydataset.addValue(val, "status", key.toLowerCase());
-            }
-        }
-        return defaultcategorydataset;
-    }
+	@RequestMapping(value = "/24h/statistics.xhtml", method = RequestMethod.GET)
+	public String TrainingModuleStatistics(Model model) {
+		model.addAttribute("stat", baseDAO.object("statistics.total.status", Stats.class));
+		model.addAttribute("statistics", baseDAO.objects("training.statistics", Language.class));
 
-    @RequestMapping(value = "/24h/statistics.xhtml", method = RequestMethod.GET)
-    public String TrainingModuleStatistics(Model model) {
-        model.addAttribute("stat", baseDAO.object("statistics.total.status", Stats.class));
-        model.addAttribute("statistics", baseDAO.objects("training.statistics", Language.class));
+		model.addAttribute("graph", createDataset("statistics.status"));
 
+		UserClassificationStats ucs = userDAO.getTotalClassifications();
+		model.addAttribute("classif", ucs);
+		return "/24h/statistics";
+	}
 
-        model.addAttribute("graph",createDataset("statistics.status"));
+	@RequestMapping(value = "/contest/cstatistics.xhtml", method = RequestMethod.GET)
+	public String ContestStatistics(Principal principal, Model model, @RequestParam("cid") Integer cid) {
+		Contest contest = contestDAO.loadContest(cid);
+		Integer uid = getUid(principal);
+		boolean uidInContest = baseDAO.bool("exist.user.in.contest", uid, cid);
+		model.addAttribute("showStats", (uidInContest && contest.isShow_stats()) || contest.isShow_stats_out());
+		contestDAO.unfreezeIfNecessary(contest);
+		model.addAttribute("stat", baseDAO.object("statistics.contest.total.status", Stats.class, contest.getCid()));
+		model.addAttribute("statistics", baseDAO.objects("statistics.contest", Language.class, contest.getCid()));
+		contest.setUsers(userDAO.loadContestUsers(contest.getCid()));
+		model.addAttribute("contest", contest);
+		model.addAttribute("cuser", contest.getUsers().size());
+		model.addAttribute("ccountry", contestDAO.getCantCountry(cid));
+		model.addAttribute("cinst", contestDAO.getCantInst(cid));
+		model.addAttribute("cuserguest", contestDAO.getCantUserGuest(cid, contest.getGuestGroup()));
+		model.addAttribute("ccountryguest", contestDAO.getCantCountryGuest(cid, contest.getGuestGroup()));
+		model.addAttribute("cinstguest", contestDAO.getCantInstGuest(cid, contest.getGuestGroup()));
+		return "/contest/cstatistics";
+	}
 
-        UserClassificationStats ucs = userDAO.getTotalClassifications();
-        model.addAttribute("classif",ucs);
-        return "/24h/statistics";
-    }
+	/*
+	 * RF97 Ver estadísticas de concursos
+	 */
+	@RequestMapping(value = "/contest/globalstatistics.xhtml", method = RequestMethod.GET)
+	public String GlobalContestStatistics(Model model) {
+		model.addAttribute("stat", baseDAO.object("statistics.global.total.status", Stats.class));
+		model.addAttribute("statistics", baseDAO.objects("statistics.global", Language.class));
+		model.addAttribute("graph", createDataset("statistics.global.total.status"));
+		return "/contest/globalstatistics";
+	}
 
-    @RequestMapping(value = "/contest/cstatistics.xhtml", method = RequestMethod.GET)
-    public String ContestStatistics(Principal principal,Model model, @RequestParam("cid") Integer cid) {
-        Contest contest = contestDAO.loadContest(cid);
-        Integer uid = getUid(principal);
-        boolean uidInContest = baseDAO.bool("exist.user.in.contest",uid,cid	);
-        model.addAttribute("showStats", (uidInContest && contest.isShow_stats()) || contest.isShow_stats_out());
-        contestDAO.unfreezeIfNecessary(contest);
-        model.addAttribute("stat", baseDAO.object("statistics.contest.total.status", Stats.class, contest.getCid()));
-        model.addAttribute("statistics", baseDAO.objects("statistics.contest", Language.class, contest.getCid()));
-        contest.setUsers(userDAO.loadContestUsers(contest.getCid()));
-        model.addAttribute("contest", contest);
-        model.addAttribute("cuser",contest.getUsers().size());
-        model.addAttribute("ccountry",contestDAO.getCantCountry(cid));
-        model.addAttribute("cinst",contestDAO.getCantInst(cid));
-        model.addAttribute("cuserguest",contestDAO.getCantUserGuest(cid,contest.getGuestGroup()));
-        model.addAttribute("ccountryguest",contestDAO.getCantCountryGuest(cid, contest.getGuestGroup()));
-        model.addAttribute("cinstguest",contestDAO.getCantInstGuest(cid, contest.getGuestGroup()));
-        return "/contest/cstatistics";
-    }
-
-    /*
-    * RF97 Ver estadísticas de concursos
-    * */
-    @RequestMapping(value = "/contest/globalstatistics.xhtml", method = RequestMethod.GET)
-    public String GlobalContestStatistics(Model model) {
-        model.addAttribute("stat", baseDAO.object("statistics.global.total.status", Stats.class));
-        model.addAttribute("statistics", baseDAO.objects("statistics.global", Language.class));
-        model.addAttribute("graph",createDataset("statistics.global.total.status"));
-        return "/contest/globalstatistics";
-    }
-
-    @RequestMapping(value = "/practice/virtualstatistics.xhtml", method = RequestMethod.GET)
-    public String VirtualStatistics(Model model) {
-        model.addAttribute("stat", baseDAO.object("statistics.virtual.total.status", Stats.class));
-        model.addAttribute("statistics", baseDAO.objects("statistics.virtual", Language.class));
-        model.addAttribute("graph",createDataset("statistics.virtual.status"));
-        return "/practice/virtualstatistics";
-    }
+	@RequestMapping(value = "/practice/virtualstatistics.xhtml", method = RequestMethod.GET)
+	public String VirtualStatistics(Model model) {
+		model.addAttribute("stat", baseDAO.object("statistics.virtual.total.status", Stats.class));
+		model.addAttribute("statistics", baseDAO.objects("statistics.virtual", Language.class));
+		model.addAttribute("graph", createDataset("statistics.virtual.status"));
+		return "/practice/virtualstatistics";
+	}
 
 }
